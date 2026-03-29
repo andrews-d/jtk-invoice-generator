c6482be915afc788151bdb0e4c747e264f7eb449
Andrews <andrewsdevaraj53561@gmail.com>
2026-03-29 16:59:04 +0530

    Invoice Generation with search Values

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://jtk-invoice-generator-back-end.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
